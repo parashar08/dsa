44102bf641daf7382f7457a40a8d9e2a63ad9e32
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-05-10 18:17:00 +0530

    update 10

--- patterns/10.cpp
+++ patterns/10.cpp
@@ -5,14 +5,23 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<i+1; j++) {
-            cout << "* ";
-        }
-        cout << "\n";
-    }
-    for (int i=n-1; i>0; i--) {
-        for (int j=0; j<i; j++) {
+    // for (int i=0; i<n; i++) {
+    //     for (int j=0; j<i+1; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << "\n";
+    // }
+    // for (int i=n-1; i>0; i--) {
+    //     for (int j=0; j<i; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    for (int i=1; i<=2*n-1; i++) {
+        int stars = i;
+        if (i > n) stars = 2*n-i;
+        for (int j=1; j<=stars; j++) {
             cout << "* ";
         }
         cout << "\n";
